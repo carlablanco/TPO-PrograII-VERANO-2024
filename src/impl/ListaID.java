f716a6c9d80bf554a62ab629a5cdb888bc0a6ed6
Carla <88563106+carlablanco@users.noreply.github.com>
2024-02-13 18:41:06 -0300

    implementaciones

--- src/impl/ListaID.java
+++ src/impl/ListaID.java
@@ -3,46 +3,96 @@ package impl;
 import api.ListaTDA;
 
 public class ListaID implements ListaTDA {
+	private Nodo inicio;
+	private Nodo ultimo;
 
 	@Override
 	public void Inicializar() {
-		// TODO Auto-generated method stub
-		
+        inicio = null;
+        ultimo = null;
 	}
 
 	@Override
 	public void Agregar(int x) {
-		// TODO Auto-generated method stub
+		Nodo nuevoNodo = new Nodo();
+		nuevoNodo.valor = x;
+		nuevoNodo.siguiente = null;
 		
+		// Si la lista estaba vacia
+		if (inicio == null) {
+			ultimo = nuevoNodo;
+			inicio = ultimo;
+		}
+		// Si la lista tenia items
+		if (ultimo != null) {
+			ultimo.siguiente = nuevoNodo;
+			ultimo = nuevoNodo;
+		}
 	}
 
+	
 	@Override
 	public void Eliminar(int x) {
-		// TODO Auto-generated method stub
+		// Elimina solo la primera aparicion o todas?
+		Nodo actual = inicio;
 		
+		if (actual.valor == x) {
+			Eliminar();
+		}
+		else {
+		
+		while (actual.siguiente != null && actual.siguiente.valor != x) {
+			actual = actual.siguiente;
+		}
+		if (actual.siguiente != null) {
+			actual.siguiente = actual.siguiente.siguiente;
+		}
+		}
 	}
 	
 	@Override
 	public void Eliminar() {
-		// TODO Auto-generated method stub
-		
+		if (inicio != null) {
+			inicio = inicio.siguiente;
+		}
 	}
 
 	@Override
 	public int Recuperar(int indice) {
 		// TODO Auto-generated method stub
-		return 0;
+		Nodo actual = inicio;
+		int count = 0;
+		while (actual != null && count < indice) {
+			actual = actual.siguiente;
+			count++;
+		}
+		return actual.valor;
 	}
 
 	@Override
 	public int Len() {
-		// TODO Auto-generated method stub
-		return 0;
+		int count = 0;
+		Nodo actual = inicio;
+		while (actual != null) {		
+			count++;
+			actual = actual.siguiente;
+		}
+		return count;
 	}
 
+	
 	@Override
 	public boolean Existe(int x) {
 		// TODO Auto-generated method stub
+		Nodo actual = inicio;
+		
+		while (actual != null) {
+			if (actual.valor == x) {
+				return true;
+			} else {
+				actual = actual.siguiente;
+			}		
+		}
 		return false;
 	}
 
--- src/main/TPO.java
+++ src/main/TPO.java
@@ -6,8 +6,19 @@ import impl.ListaID;
 public class TPO {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ListaTDA lista = new ListaID();
+        lista.Inicializar();
+        lista.Agregar(24);
+        lista.Agregar(80);
+        lista.Agregar(45);
+        System.out.println("este es el len de agregar"+lista.Len());
+        
+        lista.Eliminar(80);
+        System.out.println("este es el len post eliminar"+lista.Len());
+        
+        System.out.println("recuperado"+lista.Recuperar(1));
+        System.out.println("existe?"+lista.Existe(80));
+        
 	}
-
+	
 }
